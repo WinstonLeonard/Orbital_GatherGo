f6add35bb3afd633667569221349e1b2d6634a8c
Winston <64679685+WinstonLeonard@users.noreply.github.com>
2023-06-07 00:50:20 +0700

    updated upload pfp

--- screens/Uploadpfp.js
+++ screens/Uploadpfp.js
@@ -1,15 +1,32 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity, Image, Alert, ScrollView, KeyboardAvoidingView } from 'react-native';
 import CustomButton from '../shared/button';
+import * as ImagePicker from 'expo-image-picker';
 import { authentication, db } from '../firebase/firebase-config';
 import { collection, addDoc, doc, setDoc} from "firebase/firestore"; 
 
 export default function Uploadpfp({navigation}) {
-
+    const [image, setImage] = useState(require('../assets/pictures/uploadPfp.jpg'));
 
     const next = () => {
         console.log('next');
     }
+
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+    
+        console.log(result);
+    
+        if (!result.canceled) {
+            setImage({ uri: result.assets[0].uri });
+        }
+      };
   
 
     return(
@@ -25,8 +42,8 @@ export default function Uploadpfp({navigation}) {
 
 
             <View style = {styles.imageContainer}>
-            <TouchableOpacity>
-            <Image source = {require('../assets/pictures/upload pfp logo.png')}
+            <TouchableOpacity onPress = {pickImage}>
+            <Image source = {image}
                     style = {styles.imageStyle}
                     resizeMode='contain'
                    ></Image>
@@ -81,6 +98,8 @@ const styles = StyleSheet.create({
     imageStyle: {
         width: 250,
         height: 250,
+        borderRadius: 1000,
+        borderColor: 'black',
     },
       buttonContainer: {
         position: 'absolute',
