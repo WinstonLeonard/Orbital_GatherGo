789cfb9f20f1217e9c4e9b2e9af43a8402c73aad
Jason Lienardi <76238813+jasonlienardi@users.noreply.github.com>
2023-06-07 20:19:27 +0700

    New homescreen (no homebar navigation)

--- screens/Home.js
+++ screens/Home.js
@@ -1,33 +1,61 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity, Image } from 'react-native';
-import { authentication } from '../firebase/firebase-config';
+import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity, Image, ImageBackground } from 'react-native';
+import { authentication, db } from '../firebase/firebase-config';
+import { collection, doc, getDoc } from 'firebase/firestore';
 import CustomButton from '../shared/button';
 
 
 export default function  Home({navigation}) {
 
+    const image = require("../assets/pictures/Homescreen.png")
 
-    const handleLogout = () => {
-        authentication.signOut()
-        .then(() => {
-            navigation.replace('NewLogin')
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorMessage)
-            // ..
-          })
-    }
+    // const handleLogout = () => {
+    //     authentication.signOut()
+    //     .then(() => {
+    //         navigation.replace('NewLogin')
+    //     })
+    //     .catch((error) => {
+    //         const errorCode = error.code;
+    //         const errorMessage = error.message;
+    //         console.log(errorMessage)
+    //         // ..
+    //       })
+    // }
 
     return (
-        <View style = {styles.container}> 
-            <Text style = {styles.title}> Home Screen</Text>
-            <Text style = {styles.title}> Welcome... </Text>
-            <Text style = {styles.title}> {authentication.currentUser?.email} </Text>
-            <Text style = {styles.title}> Home screen to be continued... </Text>
-            <CustomButton text = "Logout" onPress={handleLogout}></CustomButton>
-        </View>
+        <ImageBackground source={image} resizeMode = 'cover' style={styles.image}>
+            <View style = {styles.container}> 
+                    <Text style = {styles.title}> Hello, </Text>
+                    <Text style = {styles.title}> Evan Wijaya </Text>
+            </View>
+
+            <View style = {styles.signInOptions}>
+                <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => console.log("Button pressed")}
+                    >
+                    <Image
+                    source={require('../assets/pictures/nearby.png')}
+                    style={styles.icons}
+                    />
+                </TouchableOpacity> 
+
+                <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => console.log("Button pressed")}
+                    >
+                    <Image
+                    source={require('../assets/pictures/expenses.png')}
+                    style={styles.icons}
+                    />
+                </TouchableOpacity> 
+            </View>
+
+            <View style = {styles.UpcomingEvents}> 
+                    <Text style = {styles.title}> Up-coming Events: </Text>
+            </View>
+
+        </ImageBackground>
     )
 
 }
@@ -35,19 +63,36 @@ export default function  Home({navigation}) {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        paddingTop: 50,
-        paddingHorizontal: 10,
-        //justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#292929',
+        marginTop: 80,
+        marginBottom: 50,
+        width: '80%'
     },
     title: {
-        marginBottom: 10,
-        fontSize: 20,
-        color: 'cyan',
+        textAlign: 'left',
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 28,
     },
-    imageContainer: {
-        //backgroundColor: 'red',
+    image: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    button: {
+
+    },
+    signInOptions: {
+        flexDirection: 'row', // Arrange buttons horizontally
+        justifyContent: 'space-evenly' , // Add equal spacing between buttons
+        width: '100%', // Take full width of the container      
+    },
+    icons: {
+        width: 170,
+        height: 170,
+        resizeMode: 'cover',
+    },
+    UpcomingEvents: {
+        marginTop: 190,
+        marginBottom: 50,
+        width: '80%'
     }
 })
